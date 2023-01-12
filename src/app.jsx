703d912d2ce4ce58e40b54b07d879bde1a19806c
Mateo Covacho Berrocal <mateocovachov2@gmail.com>
2023-01-12 12:49:59 +0100

    add footer attempt

--- src/app.jsx
+++ src/app.jsx
@@ -833,7 +833,47 @@ function App() {
         </Row>
       </section>
       <div>
-        <footer style={{ position: "absolute" }} ref={contact} className='container-fluid w-100 p-5'>
+      <footer class="w-100 py-4 flex-shrink-0">
+        <div class="container py-4">
+            <div class="row gy-4 gx-5">
+                <div class="col-lg-4 col-md-6">
+                    <h5 class="h1 text-white">FB.</h5>
+                    <p class="small text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+                    <p class="small text-muted mb-0">&copy; Copyrights. All rights reserved. <a class="text-primary" href="#">Bootstrapious.com</a></p>
+                </div>
+                <div class="col-lg-2 col-md-6">
+                    <h5 class="text-white mb-3">This page</h5>
+                    <ul class="list-unstyled text-muted">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Get started</a></li>
+                        <li><a href="#">FAQ</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-2 col-md-6">
+                    <h5 class="text-white mb-3">Quick links</h5>
+                    <ul class="list-unstyled text-muted">
+                        <li><a target='_blank' classname='m-auto' href='mailto: mateocovacho@gmail.com'>Mail</a></li>
+                        <li><a href="https://www.linkedin.com/in/mateo-covacho-berrocal-35a039224/">Linkedin</a></li>
+                        <li><a href="https://twitter.com/covacho_dev">Twitter</a></li>
+                        <li><a href="https://stackoverflow.com/users/18017427/mateo-covacho">StacOoverflow</a></li>
+                        <li><a href="https://github.com/mateo-covacho">Github</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <h5 class="text-white mb-3">Newsletter</h5>
+                    <p class="small text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+                    <form action="#">
+                        <div class="input-group mb-3">
+                            <input class="form-control" type="text" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+                            <button class="btn btn-primary" id="button-addon2" type="button"><i class="fas fa-paper-plane"></i></button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </footer>
+       <footer style={{ position: "absolute" }} ref={contact} className='container-fluid w-100 p-5'>
           <>
             {/*Section heading*/}
             <h2 className='display-3 wrap mx-auto mb-5 '>Contact me</h2>
